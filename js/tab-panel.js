ed877294fbd24ee4954da3efb2a31a0e87af186f
Matt Ossman <matt.ossman@gmail.com>
2016-08-26 21:18:06 -0400

    Remove jQuery in favor of vanilla JS

--- js/tab-panel.js
+++ js/tab-panel.js
@@ -1,62 +1,71 @@
-$(document).ready(function(){
-	var SWIPE_THRESHOLD = 300,
-		MAX_OFFSET_PERCENT = 20,
-		tabPanel = $('.tab-panel'),
-		tabButtons = tabPanel.find('.tab-button'),
-		tabs = tabPanel.find('.tab'),
+HTMLCollection.prototype.forEach = Array.prototype.forEach;
+HTMLCollection.prototype.indexOf = Array.prototype.indexOf;
+
+var SWIPE_THRESHOLD = 300,
+	MAX_OFFSET_PERCENT = 20;
+
+window.onload = function(){
+	var tabPanel = document.getElementById('tab-panel'),
+		tabButtons = tabPanel.getElementsByClassName('tab-button'),
+		tabs = tabPanel.getElementsByClassName('tab'),
 		activeIndex = 0;
 
-	tabButtons.click(function(){
-		var selectedIndex = tabButtons.index(this);
-		activateTab(selectedIndex);
+	tabButtons.forEach(function(button){
+		button.onclick = function(){
+			var selectedIndex = tabButtons.indexOf(this);
+			activateTab(selectedIndex);
+		};
 	});
 
 	function activateTab(index){
 		activeIndex = index;
-		tabButtons.removeClass('active');
-		tabs.removeClass('active');
-		$(tabButtons.get(index)).addClass('active');
-		$(tabs.get(index)).addClass('active');
+		for(var i=0; i<tabButtons.length; i++){
+			tabButtons[i].className = (i == activeIndex) ? 'tab-button active' : 'tab-button';
+		}
 		snapToActiveTab();
 	}
 
 	function incrementActiveTab(incrementBy){
 		var newIndex = activeIndex+incrementBy;
-		if(newIndex >= 0 && newIndex < tabs.size()){
+		if(newIndex >= 0 && newIndex < tabs.length){
 			activateTab(newIndex);
 		}
 	}
 
 	function snapToActiveTab(){
-		tabs.each(function(index, tab){
-			var leftPosition = (index-activeIndex)*100;
-			$(tab).css({left: leftPosition+'%'});
+		tabs.forEach(function(tab){
+			var index = tabs.indexOf(tab),
+				leftPosition = (index-activeIndex)*100;
+			tab.style = 'left:'+leftPosition+'%';
 		});
 	}
 
 	function offsetTabs(offset){
-		tabs.each(function(index, tab){
-			var leftPosition = (index-activeIndex)*100 +
+		tabs.forEach(function(tab){
+			var index = tabs.indexOf(tab),
+				leftPosition = (index-activeIndex)*100 +
 				Math.floor(offset > 0 ?
 					Math.min(offset/5, MAX_OFFSET_PERCENT) :
 					Math.max(offset/5, -MAX_OFFSET_PERCENT)
 				);
-			$(tab).css({left: leftPosition+'%'});
+			tab.style = 'left:'+leftPosition+'%';
 		});
 	}
 
 	function enableSwiping(allowOffset){
 		var swipeStart, isSwiping;
-		tabPanel.mousedown(function(e){
+		tabPanel.onmousedown = function(e){
 			swipeStart = e.clientX;
 			isSwiping = true;
-		});
+		};
 
-		allowOffset && tabPanel.mousemove(function(e){
-			isSwiping && offsetTabs(e.clientX - swipeStart);
-		});
+		if(allowOffset){
+			tabPanel.onmousemove = function(e){
+				isSwiping && offsetTabs(e.clientX - swipeStart);
+			};
+		}
 
-		$(document.body).mouseup(function(e){
+		document.body.onmouseup = function(e){
 			var distance = e.clientX - swipeStart;
 			if(distance > SWIPE_THRESHOLD){
 				incrementActiveTab(-1);
@@ -66,9 +75,9 @@ $(document).ready(function(){
 			}
 			isSwiping = false;
 			snapToActiveTab();
-		});
+		};
 	}
 
 	snapToActiveTab();
 	enableSwiping(true);
-});
+};
